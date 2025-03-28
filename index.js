8210863579eea3111465bfb62dc3b00047880dc0
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2025-03-28 15:17:43 +0100

    funktioniert noch nicht

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express'); // import express
+const app = express(); // as an app
+const port = 9000; // set port
+const fs = require('fs'); // import fs
+app.use(express.json()); // Middleware to write in body
+// Helpfunction
+function readFile() {
+    const data = fs.readFileSync("chars.json", "utf8");
+    return JSON.parse(data);
+}
+function writeFile(data) {
+    fs.writeFileSync('chars.json', JSON.stringify(data, null, 2));  // JSON.stringify converts Java object to JSON format
+}
+app.get("/chars", (req, res) => {
+    res.json(readFile());
+});
+app.post("/chars", (req, res) => {
+    const chars = readFile(); // read from file
+    const { charClass, name } = req.body;
+   if(name && art){
+        const newChar = {
+            id: tiere.length + 1,
+            charClass: charClass,
+            name: name
+        }
+    }
+    else{
+        res.send("Data incomplete")
+    }
+    chars.push(newChar)
+    writeFile(chars); // write to file
+    res.json(201).json(newChar);
+});
+app.put("/chars/:id", (req, res) => {
+    const id = req.params.id;
+    const chars = readFile(); // read from file
+    const newName = req.body.name;
+    const foundChar = chars.find(char => char.id == id);
+    foundChar.name = newName;
+    res.json(foundChar);
+    writeFile(chars); // write to file
+});
+app.delete("/chars/:id", (req, res) => {
+    const id = req.params.id;
+    const chars = readFile(); // read from file
+    const index = chars.findIndex(char => char.id == id);
+    const deletedChar = chars.splice(index, 1);
+    writeFile(chars); // write to file
+    res.json("Deleted " + deletedChar[0].name)
+});
+app.listen(9000, () => {
+    console.log("Server läuft auf http://localhost:9000/chars")
+}); 
